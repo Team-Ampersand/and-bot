7d4b7318a12431731f12b923824d3879149b4b15
aesperer <piyrw9754@gmail.com>
2024-01-07 22:44:38 +0900

    start project

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+
+from discord.ext import tasks
+import datetime
+import time
+
+TOKEN = 'MTE5MzU0NDg5NDExMTgyMTg0NA.GoIcP6.1Xg9Bu9CROeY1h8mtagHOJ0pdgr3y4CFSORDz8'
+CHANNEL_ID = 'https://discord.com/api/oauth2/authorize?client_id=1193544894111821844&permissions=8&scope=bot'
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+client = discord.Client(intents=intents)
+
+@tasks.loop(minutes=1)
+async def study_time_notification():
+    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    if(now.hour == 9 and now.minute == 0):
+        channel = client.get.channel(CHANNEL_ID)
+        role = discord.utils.get(channel.guild.roles, name ="6기-server")
+        await channel.send(f"{role.mention} 스터디 데일리를 작성하실 시간입니다! 오늘 공부할 주제, 가장 궁금한 점, 어제 공부한 내용을 적어주세요")
+
+@client.event
+async def on_ready():
+    study_time_notification.start()
+
+client.run(TOKEN)
+
